d5bc8edc415c3ab5c7d9c341e86b4f16c7ef0525
AneuryEstevez <87092561+AneuryEstevez@users.noreply.github.com>
2023-11-06 13:40:22 -0400

    search screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,6 +4,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:pokedex/detail_screen.dart';
+import 'package:pokedex/search_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,101 +18,221 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List pokedex = [];
   ScrollController _scrollController = ScrollController();
+  bool isFirstTime = true;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+
+    // Check if it's the first time the app is opened or SharedPreferences is empty.
+    checkFirstTime().then((firstTime) {
+      if (firstTime) {
+        print('First time!');
+        // Fetch and save all Pokémon data from the API.
+        fetchAndSaveAllPokemonData().then((success) {
+          if (success) {
+            setState(() {
+              isFirstTime = false;
+            });
+          }
+        });
+      } else {
+        print('Not first time!');
+      }
+    });
+
     if(mounted) {
       fetchPokemonData();
       _scrollController.addListener(_scrollListener);
     }
   }
 
+  Future<bool> checkFirstTime() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('isFirstTime') ?? true;
+  }
+
+  Future<bool> fetchAndSaveAllPokemonData() async {
+    try {
+      var url = Uri.https("pokeapi.co", "/api/v2/pokemon", {"limit": "1500"});
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final decodedJson = jsonDecode(response.body);
+        List newPokemon = decodedJson['results'];
+
+        final prefs = await SharedPreferences.getInstance();
+
+        for (var pokemonData in newPokemon) {
+          String name = pokemonData['name'];
+          String url = pokemonData['url'];
+          List<String> parts = url.split('/');
+          String pokemonId = parts[parts.length - 2];
+
+          // Save the data
+          prefs.setString(pokemonId, name);
+        }
+
+        await prefs.setBool('isFirstTime', false);
+
+        return true;
+      } else {
+        // Handle API request failure.
+        return false;
+      }
+    } catch (e) {
+      // Handle errors
+      return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
-        appBar: AppBar(
-          // leading: Icon(Icons.menu),
-          title: Text('Pokedex'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {},
-            ),],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
+        // appBar: AppBar(
+        //   backgroundColor: Colors.white,
+        //   elevation: 0,
+        // ),
+        body: SafeArea(
+          child: Stack(
             children: [
-              Expanded(child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1.4,
-              ),controller: _scrollController,
-                itemCount: pokedex.length,
-                itemBuilder: (context, index) {
-                  String url = pokedex[index]['url'];
-                  List<String> parts = url.split('/');
-                  String pokemonId = parts[parts.length - 2];
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen(id:pokemonId)));
-                        print("ID: $pokemonId"); // Print the ID when the card is clicked
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.green,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                          child: Stack(
-                            children: [
-                              Center(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Expanded(
-                                      child: CachedNetworkImage(
-                                        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png',
-                                        placeholder: (context, url) => CircularProgressIndicator(),
-                                        errorWidget: (context, url, error) => Image.asset('images/pokeball.png'),
-                                      ),
-                                    ),
-                                    Text(
-                                      pokedex[index]['name'],
-                                      style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ],
-                                ),
+              Positioned(
+                // top: MediaQuery.of(context).viewPadding.top,
+                left: 0,
+                right: 0,
+                height: 100,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            IconData(0xef36, fontFamily: 'MaterialIcons'),
+                            size: 50,
+                            color: Colors.red,
+                          ),
+                          Text('Pokedex', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                          Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              child: IconButton(
+                                onPressed: () {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) => SearchScreen()));
+                                },
+                                icon: Icon(Icons.search, color: Colors.red),
                               ),
-                              Positioned(
-                                top: 5,
-                                right: 5,
-                                child: Text(
-                                  "#${pokemonId}",
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle, // Circular shape
+                                color: Colors.white,
+                                border: Border.all(
+                                  color: Colors.red, // Border color
+                                  width: 1.0, // Border width
+                                ),// Button color
+                              ),
+
+                            ),
+                          ),
+                          Container(
+                            child: IconButton(
+                              onPressed: () {
+
+                              },
+                              icon: Icon(Icons.filter_list, color: Colors.red),
+                            ),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle, // Circular shape
+                              color: Colors.white,
+                              border: Border.all(
+                                color: Colors.red, // Border color
+                                width: 1.0, // Border width
+                              ),// Button color
+                            ),
+
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                bottom: 0,
+                width: width,
+                child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.4,
+                ),controller: _scrollController,
+                  itemCount: pokedex.length,
+                  itemBuilder: (context, index) {
+                    String url = pokedex[index]['url'];
+                    List<String> parts = url.split('/');
+                    String pokemonId = parts[parts.length - 2];
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen(id:pokemonId)));
+                          print("ID: $pokemonId"); // Print the ID when the card is clicked
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                            child: Stack(
+                              children: [
+                                Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Expanded(
+                                        child: CachedNetworkImage(
+                                          imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png',
+                                          placeholder: (context, url) => CircularProgressIndicator(),
+                                          errorWidget: (context, url, error) => Image.asset('images/pokeball.png'),
+                                        ),
+                                      ),
+                                      Text(
+                                        pokedex[index]['name'],
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                              ),
-                            ],
+                                Positioned(
+                                  top: 5,
+                                  right: 5,
+                                  child: Text(
+                                    "#${pokemonId}",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  );
+                    );
 
-                },
-              ))
+                  },
+                ),
+              ),
             ],
           ),
         )
---
+++ lib/search_screen.dart
@@ -0,0 +1,228 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'detail_screen.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  List<String> searchResults = [];
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(() {
+      updateSearchResults(_searchController.text);
+    });
+  }
+
+  Future<void> updateSearchResults(String query) async {
+    if (query.isNotEmpty) {
+      final prefs = await SharedPreferences.getInstance();
+      final allPokemonData = prefs.getKeys();
+
+      List<String> matchingPokemon = allPokemonData.where((pokemonId) {
+        final value = prefs.get(pokemonId);
+
+        if (value is String) {
+          // Check if the query matches a Pokemon name or ID.
+          final nameLower = value.toLowerCase();
+          if (nameLower.contains(query.toLowerCase())) {
+            return true; // Matches the Pokemon name.
+          } else if (pokemonId.contains(query)) {
+            return true; // Matches the Pokemon ID.
+          }
+        }
+
+        return false;
+      }).toList();
+
+      // Sort the search results by Pokémon ID
+      matchingPokemon.sort((a, b) => int.parse(a).compareTo(int.parse(b)));
+
+      // Update the search results.
+      setState(() {
+        searchResults = matchingPokemon;
+      });
+  }
+  }
+
+  // Function to get the Pokémon name from SharedPreferences
+  Future<String> getPokemonName(String pokemonId) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(pokemonId) ?? 'Unknown';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Positioned(
+                // top: MediaQuery.of(context).viewPadding.top,
+                left: 0,
+                right: 0,
+                height: 150,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            IconData(0xef36, fontFamily: 'MaterialIcons'),
+                            size: 50,
+                            color: Colors.red,
+                          ),
+                          Text('Pokedex', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.arrow_back, color: Colors.red,),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: TextField(
+                                  autofocus: true,
+                                  controller: _searchController,
+                                  decoration: InputDecoration(
+                                    hintText: 'Search Pokemon by name or ID',
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30.0),
+                                    ),
+                                  ),
+                                  // Add text field controllers and handlers as needed
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: IconButton(
+                                onPressed: () {
+                                  // Handle filter button click here
+                                },
+                                icon: Icon(Icons.filter_list, color: Colors.red),
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle, // Circular shape
+                                color: Colors.white,
+                                border: Border.all(
+                                  color: Colors.red, // Border color
+                                  width: 1.0, // Border width
+                                ),// Button color
+                              ),
+
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 150,
+                bottom: 0,
+                width: width,
+                child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 1.4,
+                ),
+                  itemCount: searchResults.length,
+                  itemBuilder: (context, index) {
+                    String pokemonId = searchResults[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => DetailScreen(id:pokemonId)));
+                          print("ID: $pokemonId");
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.green,
+                          ),
+                          child: FutureBuilder(
+                            future: getPokemonName(searchResults[index]),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState == ConnectionState.waiting) {
+                                return CircularProgressIndicator(); // Show a loading indicator while fetching the name.
+                              } else if (snapshot.hasError) {
+                                return Text('Error'); // Handle errors, if any.
+                              } else {
+                                final pokemonName = snapshot.data.toString();
+                                return Padding(
+                                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                                  child: Stack(
+                                    children: [
+                                      Center(
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Expanded(
+                                              child: CachedNetworkImage(
+                                                imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${searchResults[index]}.png',
+                                                placeholder: (context, url) => CircularProgressIndicator(),
+                                                errorWidget: (context, url, error) => Image.asset('images/pokeball.png'),
+                                              ),
+                                            ),
+                                            Text(
+                                              pokemonName, // Display the fetched Pokémon name
+                                              style: const TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Positioned(
+                                        top: 5,
+                                        right: 5,
+                                        child: Text(
+                                          "#${searchResults[index]}",
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                    );
+
+                  },
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
