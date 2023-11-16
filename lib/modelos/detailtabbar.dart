e93bddc45a583a07d0c6dd45218397cc5992fb3d
jorgetuma <jetm0001@ce.pucmm.edu.do>
2023-11-15 20:20:47 -0400

    Mejora de la estetica de las estadisticas

--- lib/modelos/detailtabbar.dart
+++ lib/modelos/detailtabbar.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/api/apiservice.dart';
 import 'package:pokedex/modelos/pokemoninfo.dart';
 
 class DetailTabBar extends StatelessWidget {
   final PokemonInfo pokemon;
 
   const DetailTabBar({super.key, required PokemonInfo this.pokemon});
-
+  
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -72,13 +73,18 @@ class DetailTabBar extends StatelessWidget {
                     Column(
                       children: pokemon.stats.map((stat) {
                         return ListTile(
+                          contentPadding: EdgeInsets.symmetric(vertical: 5),
                           title: Text(
                             '${stat.stat.name}: ${stat.baseStat}',
-                            style: TextStyle(fontWeight: FontWeight.bold),
+                            style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),
                           ),
                           subtitle: LinearProgressIndicator(
                             value: stat.baseStat /
                                 100.0, // Asumiendo un rango de 0-100
+                            backgroundColor: Colors.grey[300], // Color de fondo
+                            valueColor: AlwaysStoppedAnimation<Color>(ApiService.getInstance().getColorType(ApiService.getInstance().pokemonInfo!.types[0].type.name)), // Color de la barra de progreso
+                            minHeight: 20,
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
                           ),
                         );
                       }).toList(),
